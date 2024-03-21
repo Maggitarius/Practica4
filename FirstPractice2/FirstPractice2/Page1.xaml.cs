e48fcccccc5eed38e8f6485c202152748fb7b963
Maggitarius <144776072+Maggitarius@users.noreply.github.com>
2024-03-21 13:17:35 +0300

    Add files via upload

---
+++ FirstPractice2/FirstPractice2/Page1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstPractice2;
+
+namespace FirstPractice1
+{
+    public partial class Page1 : Page
+    {
+        private LibraryDatabaseEntities library = new LibraryDatabaseEntities();
+        public Page1()
+        {
+            InitializeComponent();
+            Authors.ItemsSource = library.Authors.ToList();
+            ComboBxA.ItemsSource = library.Authors.ToList();
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            Authors.ItemsSource = library.Authors.ToList().Where(item => item.AuthorName.Contains(Search.Text));
+        }
+
+        private void Filtr_Click(object sender, RoutedEventArgs e)
+        {
+            Authors.ItemsSource = library.Authors.ToList();
+        }
+
+        private void ComboBxA_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBxA.SelectedItem != null)
+            {
+                var sel = ComboBxA.SelectedItem as Authors;
+                Authors.ItemsSource = library.Authors.ToList().Where(item => item == sel);
+            }
+        }
+    }
+}
+
---
+++ FirstPractice2/FirstPractice2/Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstPractice2;
+
+namespace FirstPractice1
+{
+    public partial class Page2 : Page
+    {
+        private LibraryDatabaseEntities library = new LibraryDatabaseEntities();
+        public Page2()
+        {
+            InitializeComponent();
+            Books.ItemsSource = library.Books.ToList();
+        }
+
+        private void Filtr_Click(object sender, RoutedEventArgs e)
+        {
+            Books.ItemsSource = library.Books.ToList();
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            Books.ItemsSource = library.Books.ToList().Where(item => item.GenreOfbOOK.Contains(Search.Text));
+        }
+
+        private void ComboBxB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ComboBxB.SelectedItem != null)
+            {
+                var sel = ComboBxB.SelectedItem as Books;
+                Books.ItemsSource = library.Books.ToList().Where(item => item == sel);
+            }
+
+        }
+    }
+}
