d07af3e028853a5fac889d14f4e2de72eb6fc2b7
Maggitarius <144776072+Maggitarius@users.noreply.github.com>
2024-03-21 13:15:52 +0300

    Add files via upload

---
+++ FirstPractice1/FirstPractice1/Page1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstPractice1.LibraryDatabaseDataSetTableAdapters;
+
+namespace FirstPractice1
+{
+    public partial class Page1 : Page
+    {
+        AuthorsTableAdapter authors = new AuthorsTableAdapter();
+        public Page1()
+        {
+            InitializeComponent();
+            Authors.ItemsSource = authors.GetData();
+            ComboBxA.ItemsSource = authors.GetData();
+            ComboBxA.DisplayMemberPath = "AuthorSurname";
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            Authors.ItemsSource = authors.SearchData(Search.Text);
+        }
+
+        private void ComboBxA_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var id = (int)(ComboBxA.SelectedItem as DataRowView).Row[0];
+            Authors.ItemsSource = authors.SurnameSearch(id);
+        }
+
+        private void Filtr_Click(object sender, RoutedEventArgs e)
+        {
+            Authors.ItemsSource = authors.GetData();
+        }
+    }
+} 
---
+++ FirstPractice1/FirstPractice1/Page2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstPractice1.LibraryDatabaseDataSetTableAdapters;
+
+namespace FirstPractice1
+{
+    public partial class Page2 : Page
+    {
+        BooksTableAdapter books = new BooksTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            Books.ItemsSource = books.GetData();
+            ComboBxB.ItemsSource = books.GetData();
+            ComboBxB.DisplayMemberPath = "GenreOfbOOK";
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            Books.ItemsSource = books.SearchBooks(Search.Text);
+        }
+        private void ComboBxB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var id = (int)(ComboBxB.SelectedItem as DataRowView).Row[0];
+            Books.ItemsSource = books.BooksSearch(id);
+        }
+
+        private void Filtr_Click(object sender, RoutedEventArgs e)
+        { 
+            Books.ItemsSource = books.GetData();
+        }
+
+    }
+}
